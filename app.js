a387b7b0f6f484b9e16538fd6b5695734458b1a7
Agustina Rojas <agusrojasmc@gmail.com>
2025-08-02 17:16:26 -0300

    cambio para tener varios origenes para cors

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ require("dotenv").config();
 
 const sequelize = require("./database/db");
 
-const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:4200"];
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || "").split(",");
 
 app.use(
 	cors({
-		origin: allowedOrigins,
+		origin: function (origin, callback) {
+			if (!origin || allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
 		credentials: true,
 	})
 );
